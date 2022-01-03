a1dc2f5a1ea51d94658aacf670742fe353627f28
Ben Lackey <ben.lackey@neo4j.com>
2022-01-03 10:43:18 -0800

    camel case issues

--- simple/deploy.sh
+++ simple/deploy.sh
@@ -3,4 +3,4 @@
 RESOURCE_GROUP=$1
 
 az group create --name $RESOURCE_GROUP --location westus
-az group deployment create --template-file mainTemplate.json --parameters @mainTemplateParameters.json --resource-group $RESOURCE_GROUP
+az deployment group create --template-file mainTemplate.json --resource-group $RESOURCE_GROUP --parameters @mainTemplateParameters.json
