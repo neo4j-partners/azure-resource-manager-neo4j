919a9404ace28511f15ffb28ad7673a26931aa0d
Ben Lackey <ben.lackey@neo4j.com>
2022-03-28 09:38:40 +0100

    add apoc, two license keys.  need to push in order to test

--- scripts/node.sh
+++ scripts/node.sh
@@ -2,25 +2,17 @@
 
 echo "Running node.sh"
 
-adminUsername=$1
-adminPassword=$2
-uniqueString=$3
-licenseKey=$4
-location=$5
-graphDatabaseVersion=$6
-graphDataScienceVersion=$7
-bloomVersion=$8
-nodeCount=$9
-
 echo "Using the settings:"
 echo adminUsername \'$adminUsername\'
 echo adminPassword \'$adminPassword\'
 echo uniqueString \'$uniqueString\'
-echo licenseKey \'$licenseKey\'
 echo location \'$location\'
 echo graphDatabaseVersion \'$graphDatabaseVersion\'
 echo graphDataScienceVersion \'$graphDataScienceVersion\'
+echo graphDataScienceLicenseKey \'$graphDataScienceLicenseKey\'
 echo bloomVersion \'$bloomVersion\'
+echo bloomLicenseKey \'$bloomLicenseKey\'
+echo apocVersion \'$apocVersion\'
 echo nodeCount \'$nodeCount\'
 
 echo "Turning off firewalld"
@@ -40,9 +32,8 @@ echo Installing Graph Database...
 export NEO4J_ACCEPT_LICENSE_AGREEMENT=yes
 yum -y install neo4j-enterprise-${graphDatabaseVersion}
 
-echo Writing neo4j license key file...
-mkdir /etc/neo4j/license
-echo $licenseKey > /etc/neo4j/license/neo4j.license
+echo Installing unzip...
+yum -y install unzip
 
 echo Configuring network in neo4j.conf...
 sed -i 's/#dbms.default_listen_address=0.0.0.0/dbms.default_listen_address=0.0.0.0/g' /etc/neo4j/neo4j.conf
@@ -86,22 +77,33 @@ echo root@localhost.localdomain
 }
 answers | /usr/bin/openssl req -newkey rsa:2048 -keyout private.key -nodes -x509 -days 365 -out public.crt
 
-#for service in bolt https cluster backup; do
-for service in https; do
-  sed -i s/#dbms.ssl.policy.${service}/dbms.ssl.policy.${service}/g /etc/neo4j/neo4j.conf
-  mkdir -p /var/lib/neo4j/certificates/${service}/trusted
-  mkdir -p /var/lib/neo4j/certificates/${service}/revoked
-  cp private.key /var/lib/neo4j/certificates/${service}
-  cp public.crt /var/lib/neo4j/certificates/${service}
-done
+sed -i s/#dbms.ssl.policy.https/dbms.ssl.policy.https/g /etc/neo4j/neo4j.conf
+mkdir -p /var/lib/neo4j/certificates/https/trusted
+mkdir -p /var/lib/neo4j/certificates/https/revoked
+cp private.key /var/lib/neo4j/certificates/https
+cp public.crt /var/lib/neo4j/certificates/https
+
+sed -i s/#dbms.ssl.policy.bolt/dbms.ssl.policy.bolt/g /etc/neo4j/neo4j.conf
+mkdir -p /var/lib/neo4j/certificates/bolt/trusted
+mkdir -p /var/lib/neo4j/certificates/bolt/revoked
+cp private.key /var/lib/neo4j/certificates/bolt
+cp public.crt /var/lib/neo4j/certificates/bolt
 
 chown -R neo4j:neo4j /var/lib/neo4j/certificates
 chmod -R 755 /var/lib/neo4j/certificates
 
+# Logging
+sed -i s/#dbms.logs.http.enabled/dbms.logs.http.enabled/g /etc/neo4j/neo4j.conf
+sed -i s/#dbms.logs.query.enabled/dbms.logs.query.enabled/g /etc/neo4j/neo4j.conf
+sed -i s/#dbms.logs.security.enabled/dbms.logs.security.enabled/g /etc/neo4j/neo4j.conf
+sed -i s/#dbms.logs.debug.level/dbms.logs.debug.level/g /etc/neo4j/neo4j.conf
+
+mkdir -p /etc/neo4j/downloads
+
 if [[ $graphDataScienceVersion != None ]]; then
   echo Installing Graph Data Science...
-  curl https://s3-eu-west-1.amazonaws.com/com.neo4j.graphalgorithms.dist/graph-data-science/neo4j-graph-data-science-${graphDataScienceVersion}-standalone.zip -o neo4j-graph-data-science-${graphDataScienceVersion}-standalone.zip
-  unzip neo4j-graph-data-science-${graphDataScienceVersion}-standalone.zip
+  curl https://graphdatascience.ninja/neo4j-graph-data-science-${graphDataScienceVersion}.zip -o neo4j-graph-data-science-${graphDataScienceVersion}.zip
+  unzip neo4j-graph-data-science-${graphDataScienceVersion}.zip
   mv neo4j-graph-data-science-${graphDataScienceVersion}.jar /var/lib/neo4j/plugins
 fi
 
@@ -113,8 +115,46 @@ if [[ $bloomVersion != None ]]; then
 fi
 
 echo Configuring Graph Data Science and Bloom in neo4j.conf...
+sed -i s~#dbms.unmanaged_extension_classes=org.neo4j.examples.server.unmanaged=/examples/unmanaged~dbms.unmanaged_extension_classes=com.neo4j.bloom.server=/bloom,semantics.extension=/rdf~g /etc/neo4j/neo4j.conf
 sed -i s/#dbms.security.procedures.unrestricted=my.extensions.example,my.procedures.*/dbms.security.procedures.unrestricted=gds.*,bloom.*/g /etc/neo4j/neo4j.conf
-sed -i s/#dbms.security.procedures.allowlist=apoc.coll.*,apoc.load.*,gds.*/dbms.security.procedures.allowlist=apoc.coll.*,apoc.load.*,gds.*,bloom.*/g /etc/neo4j/neo4j.conf
+
+if [[ $apocVersion != None ]]; then
+  echo Installing APOC...
+  curl -L https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/${apocVersion}/apoc-${apocVersion}-all.jar -o apoc-${apocVersion}-all.jar
+  mv apoc-${apocVersion}-all.jar /var/lib/neo4j/plugins
+fi
+
+sed -i '$a ' /etc/neo4j/neo4j.conf
+sed -i '$a # Bloom and EDS license files' /etc/neo4j/neo4j.conf
+
+mkdir -p /etc/neo4j/licenses
+
+if [[ $bloomLicenseKey != None ]]; then
+  echo Writing Bloom license key file...
+  # bloom license
+  # https://neo4j.com/docs/bloom-user-guide/current/bloom-installation/installation-activation/
+  echo $bloomLicenseKey > /etc/neo4j/licenses/neo4j-bloom.license
+  echo Configuring Bloom license in neo4j.conf...
+  sed -i '$a neo4j.bloom.license_file=/etc/neo4j/licenses/neo4j-bloom.license' /etc/neo4j/neo4j.conf
+fi
+
+if [[ $graphDataScienceLicenseKey != None ]]; then
+  echo Writing GDS license key file...
+  echo $graphDataScienceLicenseKey > /etc/neo4j/licenses/neo4j-gds.license
+  echo Configuring GDS license in neo4j.conf...
+  # gds license
+  # https://neo4j.com/docs/graph-data-science/current/installation/installation-enterprise-edition/
+  sed -i '$a gds.enterprise.license_file=/etc/neo4j/licenses/neo4j-gds.license' /etc/neo4j/neo4j.conf
+fi
+
+sed -i '$a ' /etc/neo4j/neo4j.conf
+sed -i '$a # Bloom and EDS roles and permissions (updated in place)' /etc/neo4j/neo4j.conf
+
+# Bloom http whitelist
+sed -i '$a dbms.security.http_auth_allowlist=/,/browser.*,/bloom.*' /etc/neo4j/neo4j.conf
+
+# Enable security (not editing in place since baseline could change in the future)
+sed -i '$a dbms.security.procedures.allowlist=apoc.*,gds.*,bloom.*' /etc/neo4j/neo4j.conf
 
 echo Starting Neo4j...
 service neo4j start
