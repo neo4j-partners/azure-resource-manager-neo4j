7334ba127ffb5f04adde775c9d102ffe6a6a2666
Harshit Singhvi <harshit.singhvi@neotechnology.com>
2022-11-28 09:34:47 +0000

    Removing simple

--- simple/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-RESOURCE_GROUP=$1
-
-az group create --name $RESOURCE_GROUP --location westus
-az deployment group create --template-file mainTemplate.json --resource-group $RESOURCE_GROUP --parameters @mainTemplateParameters.json
