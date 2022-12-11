2d48f0136d7e5eed57c0c4cdfccdee8147dfdc99
Harshit Singhvi <harshit.singhvi@neotechnology.com>
2022-12-11 16:21:52 +0000

    Fixing readreplica4.sh

--- scripts/readreplica4.sh
+++ scripts/readreplica4.sh
@@ -92,7 +92,7 @@ get_vmss_tags() {
 set_vmss_tags() {
   installed_neo4j_version=$(/usr/bin/neo4j --version | awk -F " " '{print $2}')
   echo "Installed neo4j version is ${installed_neo4j_version}. Trying to set vmss tags"
-  resourceId=$(az vmss list --resource-group "${resourceGroup}" | jq -r '.[] | .id')
+  resourceId=$(az vmss list --resource-group "${resourceGroup}" | jq -r '.[] | .id' | grep 'read-replica-vmss')
   az tag create --tags Neo4jVersion="${installed_neo4j_version}" --resource-id "${resourceId}"
   echo "Added tag Neo4jVersion=${installed_neo4j_version}"
 }
