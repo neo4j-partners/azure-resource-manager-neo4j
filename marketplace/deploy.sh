0efbfa4af85f0424f3a67290d069e7c014bdcdc5
Ed Randall <ed.randall@neo4j.com>
2023-02-08 22:05:56 +0000

    Revert rgName and deploymentName

--- marketplace/deploy.sh
+++ marketplace/deploy.sh
@@ -1,10 +1,10 @@
 # create a resource group
-resourceGroup="edr-rg-08Feb22-2"
+resourceGroup="$1"
 location="westeurope"
 az group create -l $location -n $resourceGroup
 
 # perform the initial deployment
-deploymentName="edr-test-08Feb22-2"
+deploymentName="MyDeployment12"
 az deployment group create -g $resourceGroup -n $deploymentName \
         --template-file mainTemplate.json \
         --parameters @parameters.json
