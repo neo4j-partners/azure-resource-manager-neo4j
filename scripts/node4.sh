9615be90eeb0f0fd00511655bc3c743288e378f6
Harshit Singhvi <harshit.singhvi@neotechnology.com>
2023-02-22 09:43:59 +0000

    Replacing True/False with Yes/No

--- scripts/node4.sh
+++ scripts/node4.sh
@@ -123,7 +123,7 @@ set_yum_pkg() {
 }
 
 configure_graph_data_science() {
-  if [[ "${installGraphDataScience}" == True && "${nodeCount}" == 1 ]]; then
+  if [[ "${installGraphDataScience}" == Yes && "${nodeCount}" == 1 ]]; then
     echo "Installing Graph Data Science..."
     cp /var/lib/neo4j/products/neo4j-graph-data-science-*.jar /var/lib/neo4j/plugins
     chown neo4j:neo4j /var/lib/neo4j/plugins/neo4j-graph-data-science-*.jar
@@ -139,7 +139,7 @@ configure_graph_data_science() {
 }
 
 configure_bloom() {
-  if [[ ${installBloom} == True ]]; then
+  if [[ ${installBloom} == Yes ]]; then
     echo "Installing Bloom..."
     cp /var/lib/neo4j/products/bloom-plugin-*.jar /var/lib/neo4j/plugins
     chown neo4j:neo4j /var/lib/neo4j/plugins/bloom-plugin-*.jar
